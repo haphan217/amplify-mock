102c20fa40b4ccdeae8012b605d2edf30ed482b3
haphan217 <s3695302@rmit.edu.vn>
2021-09-14 19:36:19 +0700

    feat: be setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import Amplify, { API } from "aws-amplify";
+import { Container, Form, Button } from "react-bootstrap";
+import awsExport from "./aws-exports";
+Amplify.configure(awsExport);
 
 function App() {
+  const [book, setBook] = useState({ title: "", author: "" });
+  const addBook = async () => {
+    const data = {
+      body: {
+        title: book.title,
+        author: book.author,
+      },
+    };
+    try {
+      const res = await API.post("todoapis", "/books", data);
+      console.log({ res });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setBook({ ...book, [name]: value });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and commit to build.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <h3>Add books</h3>
+      <Form>
+        <Form.Group className="mb-3">
+          <Form.Label>Title</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter title"
+            value={book.title}
+            name="title"
+            onChange={handleInputChange}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Author</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter author"
+            name="author"
+            value={book.author}
+            onChange={handleInputChange}
+          />
+        </Form.Group>
+        <Button variant="primary" onClick={addBook}>
+          Submit
+        </Button>
+      </Form>
+    </Container>
   );
 }
 
